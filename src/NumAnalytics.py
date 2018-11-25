16ac2d342e8b1bb3ada2a04907f3e911a1da4af7
Pavel Shvarchov <shvarpa@gmail.com>
2018-11-25 18:59:38 +0200

    progress on iterJacobi, added decompose method tests

--- src/NumAnalytics.py
+++ src/NumAnalytics.py
@@ -28,34 +28,34 @@ class Polynom(object):
                 c_pow[term[1]] = 0
             c_pow[term[1]]+= term[0]
         return [c_pow[degree-i] if c_pow.get(degree-i) else 0 for i in range(degree+1)]
-    
-    
+
+
     def __init__(self,expr):
         if isinstance(expr, Polynom):
             expr = expr.polynom
         if isinstance(expr, str):
             expr = Polynom.parsePoly(expr)
         self.polynom = poly1d(expr)
-        
+
     def __str__(self):
         return str(self.polynom);
-    
+
     def eval(self,x):
         """calculates and returns p(x)"""
         return polyval(self.polynom,x)
-    
+
     def derive(self):
         """returns a Polynom class member """
         return Polynom(polyder(self.polynom))
-    
+
     def __eq__(self,other):
             return self.polynom == other.polynom
-     
+
     def getRootsByHalf(self,totalZone=(-5,5),zoneCount=89,maxError=0.01,derived = False, **kwargs):
-        
+
         def testRoot(root):
             return abs(self.eval(root)) < maxError
-        
+
         def getRoot(zone):
             fa,fb = self.eval(zone[0]),self.eval(zone[1])
 
@@ -63,7 +63,7 @@ class Polynom(object):
                 return solveRoot(zone,fa,fb)
             else:
                 return None
-            
+
         def solveRoot(zone,fa,fb):
             m = (zone[0]+zone[1])/2
             fm = self.eval(m)
@@ -73,12 +73,12 @@ class Polynom(object):
                 return solveRoot((zone[0],m), fa, fm)
             else:
                 return solveRoot((m,zone[1]), fm, fb)
-                
+
         def getZones():
             step = (totalZone[1] - totalZone[0])/zoneCount
             temp = arange(totalZone[0],totalZone[1]+1,step)
             return [(temp[i],temp[i+1]) for i in range(len(temp)-1)]
-        
+
         Zones = getZones()
         roots = []
         for zone in Zones:
@@ -91,80 +91,71 @@ class Polynom(object):
             roots.extend([root for root in self.derive().getRootsByHalf(derived=True) if testRoot(root)])
             roots.sort()
         return roots
-                
-        
+
+
 class Matrix(np.matrix):
-    def __init__(self,*arg):
-        super().__init__()
-        
-    def decomposeDLU(self,type =''):
+    def decompose(self,select =''):
         """
         D = direct/ main axis of the matrix: ([[1,2,3],[4,5,6],[7,8,9]]) => ([[1,0,0],[0,5,0],[0,0,9]])
         L = lower matrix triangle: ([[1,2,3],[4,5,6],[7,8,9]]) => ([[0,2,3],[0,0,6],[0,0,0]])
         U = upper matrix triangle: ([[1,2,3],[4,5,6],[7,8,9]]) => ([[0,0,0],[4,0,0],[7,8,0]])
-        (d+l+u)= m 
+        (d+l+u)= m
         LU = L+U
         DL = D+L
         """
         rows, cols = self.shape
         if rows!=cols:
             return None;
+        data = self.tolist()
         methods = {
             'D':(lambda:Matrix([[data[i][j] if i==j else 0 for j in range(cols)] for i in range(rows)])),
-            'U':(lambda:Matrix([[data[i][j] if j<i else 0 for j in range(cols)] for i in range(rows)])),
-            'L':(lambda:Matrix([[data[i][j] if j>i else 0 for j in range(cols)] for i in range(rows)])),
+            'U':(lambda:Matrix([[data[i][j] if i<j else 0 for j in range(cols)] for i in range(rows)])),
+            'L':(lambda:Matrix([[data[i][j] if i>j else 0 for j in range(cols)] for i in range(rows)])),
             'LU':(lambda:Matrix([[data[i][j] if i!=j else 0 for j in range(cols)] for i in range(rows)])),
-            'DL':(lambda:Matrix([[data[i][j] if i<=j else 0 for j in range(cols)] for i in range(rows)])),
+            'DL':(lambda:Matrix([[data[i][j] if i>=j else 0 for j in range(cols)] for i in range(rows)])),
             }
-        data = self.tolist()
-        if(re.fullmatch('(LU|D)',type, flags=re.IGNORECASE)):
-            return (methods['LU'](),methods['D']())
-        if(re.fullmatch('(DL|U)',type, flags=re.IGNORECASE)):
-            return (methods['DL'](),methods['U']())
-        return (methods['D'](),methods['L'](),methods['U']())
-    
+        return methods[select]()
+
     def iterSolver(self,g,h,result=None,startingGuess=None,maxError=0.01):
         matrixDegree = self.shape[0]
         startingGuess = Matrix([[0] for _ in range(matrixDegree)]) if startingGuess==None else startingGuess
         result = Matrix([[i] for i in result]) if not isinstance(result, Matrix) else result
-        
-        def iteration():
-            i,currGuess=0,startingGuess
-            while(True):
+
+        def iteration(n=25):
+            currGuess=startingGuess
+            i=1
+            while(i<n):
                 nextGuess = g*currGuess + h*result
-                if (abs(nextGuess - currGuess)<maxError):
+                if(abs(nextGuess - currGuess).max()<maxError):
                     break
+                currGuess = nextGuess
+                i+=1
+            print("done in iteration number : " , i)
             return nextGuess
-        
+
         def validate(guess):
-            return abs(result - (Matrix(self) * guess)) < maxError          
-        
+            return abs(result - (Matrix(self) * guess)).max() < maxError
+
         guess = iteration()
         return guess, validate(guess)
-    
-    def iterJacobi(self,result):
-        lu,d = self.decomposeDLU('LU')
-        dm1 = d.getI()
-        g,h = -dm1*lu, dm1
-        return self.iterSolver(g, h, result)
-    
-    def iterGaussSeidel(self,result):
-        dl,u = self.decomposeDLU('DL')
-        dlm1 = dl.getI()
-        g,h = -dlm1*u, dlm1
-        return self.iterSolver(g, h, result)
-if __name__ == '__main__':
-#     p1 = Polynom("x^2 -2")
-#     print(p1)
-#     print('-----------')
-#     print(p1.derive())
-#     print('-----------')
-#     print(p1)
-#     print('-----------')
-#     print(p1.getRootsByHalf())
-#     print('-----------')
-    m1 = Matrix('[1,2,3;4,5,6;7,8,9]')
-#     [print(x,'\n') for x in m1.decomposeDLU()]
-#     print(sum(m1.decomposeDLU()))
 
-print(m1.iterSolver(result=[1,1,1]))
+    def iterJacobi(self,result):
+        lu,d = self.decompose('LU') , self.decompose('D')
+        dInver = d**-1
+        g = -dInver*lu
+        h = dInver
+        return self.iterSolver(g, h, result)
+
+    def iterGaussSeidel(self,result):
+        dl,u = self.decompose('DL') , self.decompose('U')
+        dlInver = dl**-1
+        g = -dlInver*u
+        h = dlInver
+        return self.iterSolver(g, h, result)
+
+if __name__ == '__main__':
+    m1 = Matrix('[2,1;5,7]')
+    print(m1.iterJacobi([11,13]))
+#     print(m1.iterGaussSeidel([6,15,24]))
+
+
