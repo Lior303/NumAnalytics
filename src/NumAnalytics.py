ad9da5083d654120c26a22d8f1d68175d37a0290
Pavel Shvarchov <shvarpa@gmail.com>
2018-11-26 16:52:42 +0200

    moved polynom to different file

--- src/NumAnalytics.py
+++ src/NumAnalytics.py
@@ -3,96 +3,8 @@ Created on Oct 30, 2018
 
 @author: Pavel Shavarchov
 '''
-from numpy.lib.polynomial import poly1d, polyval,polyder
-from builtins import str
-import re
 import numpy as np
-from numpy.ma.core import arange
 from numpy.random.mtrand import random_sample
-
-class Polynom(object):
-    @staticmethod
-    def parsePoly(expr,var = r'[xX]'):
-        """ parse Polynom from text, for example: "+10 +5.3x^2 -55 x**4" is converted to the poly series [1,0,5.3,0,-45]"""
-        expr = expr.replace(' ','').replace('-','+-').replace('^','**').split('+');
-        pattern = re.compile(var)
-        terms = [pattern.split(term) for term in expr];
-        terms = [[term[0],term[1].replace('**','') if len(term)>1 else '0'] for term in terms]
-        terms = [[item + '1' if (item == '' or item == '-') else item for item in term] for term in terms]
-        c_pow = {}
-        degree = 1
-        for term in terms:
-            term = [eval(term[0]),int(term[1])]
-            if term[1]>degree:
-                degree = term[1]
-            if c_pow.get(term[1])==None:
-                c_pow[term[1]] = 0
-            c_pow[term[1]]+= term[0]
-        return [c_pow[degree-i] if c_pow.get(degree-i) else 0 for i in range(degree+1)]
-
-
-    def __init__(self,expr):
-        if isinstance(expr, Polynom):
-            expr = expr.polynom
-        if isinstance(expr, str):
-            expr = Polynom.parsePoly(expr)
-        self.polynom = poly1d(expr)
-
-    def __str__(self):
-        return str(self.polynom);
-
-    def eval(self,x):
-        """calculates and returns p(x)"""
-        return polyval(self.polynom,x)
-
-    def derive(self):
-        """returns a Polynom class member """
-        return Polynom(polyder(self.polynom))
-
-    def __eq__(self,other):
-            return self.polynom == other.polynom
-
-    def getRootsByHalf(self,totalZone=(-5,5),zoneCount=89,maxError=0.01,derived = False, **kwargs):
-
-        def testRoot(root):
-            return abs(self.eval(root)) < maxError
-
-        def getRoot(zone):
-            fa,fb = self.eval(zone[0]),self.eval(zone[1])
-
-            if(fa*fb<0):
-                return solveRoot(zone,fa,fb)
-            else:
-                return None
-
-        def solveRoot(zone,fa,fb):
-            m = (zone[0]+zone[1])/2
-            fm = self.eval(m)
-            if abs(fm) < maxError:
-                return m;
-            elif fa*fm<0:
-                return solveRoot((zone[0],m), fa, fm)
-            else:
-                return solveRoot((m,zone[1]), fm, fb)
-
-        def getZones():
-            step = (totalZone[1] - totalZone[0])/zoneCount
-            temp = arange(totalZone[0],totalZone[1]+1,step)
-            return [(temp[i],temp[i+1]) for i in range(len(temp)-1)]
-
-        Zones = getZones()
-        roots = []
-        for zone in Zones:
-            root = getRoot(zone)
-            if root:
-                roots.append(root)
-        if derived:
-            return roots;
-        elif len(roots)<self.polynom.order:
-            roots.extend([root for root in self.derive().getRootsByHalf(derived=True) if testRoot(root)])
-            roots.sort()
-        return roots
-
 
 class Matrix(np.matrix):
     def decompose(self,*selections):
@@ -122,12 +34,12 @@ class Matrix(np.matrix):
         startingGuess = Matrix([[0] for _ in range(matrixDegree)]) if startingGuess==None else startingGuess
         result = Matrix([[i] for i in result]) if not isinstance(result, np.matrix) else result
 
-        def iteration(n=25,**kwargs):
+        def iteration(n=25):
             currGuess=startingGuess
             i=1
             while(i<=n):
                 nextGuess = g*currGuess + h*result
-                if(abs(nextGuess - currGuess).max()<maxError/10):
+                if(abs(nextGuess - currGuess).max()<maxError/100):
                     currGuess = nextGuess
                     break
                 currGuess = nextGuess
@@ -136,38 +48,39 @@ class Matrix(np.matrix):
             return currGuess
 
         def validate(guess):
-            return abs(result - (Matrix(self) * guess)).max() < maxError
+            return abs(result - (self * guess)).max() < maxError
 
         guess = iteration(**kwargs)
         return guess, validate(guess)
 
     def iterJacobi(self,result,**kwargs):
-        lu,d = self.decompose('LU','D')
+        lu,d = Matrix.decompose(self,'LU','D')
         dInver = d**-1
         g = -dInver*lu
         h = dInver
-        return self.iterSolver(g, h, result,**kwargs)
+        return Matrix.iterSolver(self, g, h, result,**kwargs)
 
     def iterGaussSeidel(self,result,**kwargs):
-        dl,u = self.decompose('DL','U')
+        dl,u = Matrix.decompose(self,'DL','U')
         dlInver = dl**-1
         g = -dlInver*u
         h = dlInver
-        return self.iterSolver(g, h, result,**kwargs)
+        return Matrix.iterSolver(self, g, h, result,**kwargs)
     
     def iterSOR(self,result,w=None,**kwargs):
         w = 2*random_sample() if (w==None or w>2 or w<=0) else w
-        d,l,u = self.decompose('D','L','U')
+        d,l,u = Matrix.decompose(self,'D','L','U')
         dwlInver = (d+w*l)**-1
         g = dwlInver * ((1-w)*d -w*u)
         h = w * dwlInver
         print("w: ", w)
-        return self.iterSolver(g, h, result, **kwargs)
+        return Matrix.iterSolver(self, g, h, result, **kwargs)
         
 if __name__ == '__main__':
-    m1 = Matrix('[2,1;5,7]')
-    print(m1.iterJacobi([11,13],n=25))
-    print(m1.iterGaussSeidel([11,13]))
-    print(m1.iterSOR([11,13],n=100))
+    m1 = np.matrix('[2,1;5,7]')
+    print(Matrix.iterJacobi(m1, [11,13]))
+#     print(m1.iterJacobi([11,13],n=25))
+#     print(m1.iterGaussSeidel([11,13]))
+#     print(m1.iterSOR([11,13],n=100))
 
 
