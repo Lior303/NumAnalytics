d135c75dbe0af31296aef6beec2e3710d58e1c34
Pavel Shvarchov <Shvarpa@gmail.com>
2018-11-27 12:01:01 +0200

    changed file name from polynom to polynomial, made polynomial_approximation generic

--- src/polynomialApprox.py
+++ src/polynomialApprox.py
@@ -1,7 +1,7 @@
 import numpy as np
 # import  gaussian_elimination_method as func
 
-def polynomial_approximation(dict):
+def polynomial_approximation(dict, matrixSolver = gaussian_eliminate_result_adapter):
     def createMatrix():
         size = len(dict.keys())
         return np.matrix([[x ** i for i in range(size - 1, -1, -1)] for x in dict.keys()])
@@ -11,7 +11,13 @@ def polynomial_approximation(dict):
 
     def unpackVector(vector):
         return vector.flatten()
+    
+    matrixA , vectorB = createMatrix(), createResultsVector()
+    vectorX = matrixSolver(matrixA,resultB)
+    
+    return np.poly1d(vectorX)
 
-#     return np.poly1d(func.gaussian_eliminate_result(np.append(createMatrix(), createResultsVector(), axis=1)))
+def gaussian_eliminate_result_adapter(A,b):
+    return func.gaussian_eliminate_result(np.append(A,b, axis=1))
 
 print(polynomial_approximation({1:1, 10:0.1, 20:0.05}))
